90c4ca69925dba3497794d5830c9fe88e76bcea9
Leonardo <leonardo@asiacorp-i.com>
2020-03-07 19:03:26 +0000

    *.sql created

--- setup/create.sql
+++ setup/create.sql
@@ -1,10 +1,42 @@
-CREATE TABLE album (
-					id_album	INT,
-					isbn		VARCHAR(36),
-					art_url		VARCHAR(64),
-					artist		VARCHAR(64),
-					album		VARCHAR(64),
-					relyear		INT,
-					description	TEXT,
-					CONSTRAINT pk_album PRIMARY KEY (id_album)
+/*
+ * Application name: Collection Manager
+ * Author: Leonardo Tomé - leonardo@gmx.pt
+ * Version 1.0
+ *
+ * SQL Script: create.sql
+*/
+
+
+CREATE TABLE media_type (
+						id_type 	INT,
+						type_name 	VARCHAR(32),
+						CONSTRAINT pk_mtype PRIMARY KEY (id_type)
+);
+
+CREATE TABLE recordlabels (
+						id_label 	INT,
+						label_name 	VARCHAR(32),
+						CONSTRAINT pk_mlbl PRIMARY KEY (id_label)
+);
+
+
+CREATE TABLE albums (
+					id 				VARCHAR(32),
+					artist  		VARCHAR(64),
+					album 			VARCHAR(64),
+					relyear 		INT,
+					label 			INT,
+					media_type  	INT,
+					media_amount    INT,
+					CONSTRAINT pk_album PRIMARY KEY (id),
+					CONSTRAINT fk_label FOREIGN KEY (label) REFERENCES recordlabels(id_label),
+					CONSTRAINT fk_type  FOREIGN KEY (media_type) REFERENCES media_type(id_type)
 				);
+
+CREATE TABLE album_track_list (
+								album_id    VARCHAR(32),
+								cd_number   INT,
+								id 			INT,
+								track_name	VARCHAR(64),
+								CONSTRAINT fk_album FOREIGN KEY (album_id) REFERENCES albums(id)
+);
---
+++ setup/insert_defaults.sql
@@ -0,0 +1,24 @@
+﻿/*
+ * Application name: Collection Manager
+ * Author: Leonardo Tomé - leonardo@gmx.pt
+ * Version 1.0
+ *
+ * SQL Script: insert_defaults.sql
+*/
+
+INSERT INTO media_type (id_type,type_name) VALUES (1,"Cassette");
+INSERT INTO media_type (id_type,type_name) VALUES (2,"Compact Disc");
+
+INSERT INTO recordlabels (id_label,label_name) VALUES (1,"Unknown");
+INSERT INTO recordlabels (id_label,label_name) VALUES (2,"EMI");
+INSERT INTO recordlabels (id_label,label_name) VALUES (3,"Universal Music");
+INSERT INTO recordlabels (id_label,label_name) VALUES (4,"Sony BMG");
+INSERT INTO recordlabels (id_label,label_name) VALUES (5,"A&M Records");
+INSERT INTO recordlabels (id_label,label_name) VALUES (6,"Warner Bros.");
+INSERT INTO recordlabels (id_label,label_name) VALUES (7,"ZYX Records");
+INSERT INTO recordlabels (id_label,label_name) VALUES (8,"Epic Records");
+INSERT INTO recordlabels (id_label,label_name) VALUES (9,"Reprise Records");
+INSERT INTO recordlabels (id_label,label_name) VALUES (10,"Seven Records");
+INSERT INTO recordlabels (id_label,label_name) VALUES (11,"Virgin Records");
+INSERT INTO recordlabels (id_label,label_name) VALUES (12,"Som Livre");
+INSERT INTO recordlabels (id_label,label_name) VALUES (13,"Columbia Records");
